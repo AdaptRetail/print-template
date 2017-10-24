4555b48ec47ceae8af74793005c7a056beb0af9e
Lasse <lasse@haslev.no>
2017-10-24 22:52:45 +0200

    Changed name to snippet.js and snippet.css instead of app.js and app.css

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,6 +9,6 @@ require( '@adapt-retail/adapt-mix-extender' ).extend( mix );
  * Build script and compile sass
  * Display banner and hot reload
  */
-mix.js('src/Scripts/main.js', 'dist/app.js')
-   .sass('src/Style/main.scss', 'dist/app.css')
+mix.js('src/Scripts/main.js', 'dist/snippet.js')
+   .sass('src/Style/main.scss', 'dist/snippet.css')
    .browserSync();
