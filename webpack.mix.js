3f0cc93a7d7d26484feb6ce041524fabf33363df
Lasse <lasse@haslev.no>
2017-10-25 23:11:06 +0200

    Added  before everyting when running npm run prod

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,10 +5,32 @@ let mix = require( 'laravel-mix' );
  */
 require( '@adapt-retail/adapt-mix-extender' ).extend( mix );
 
+var buildAdaptMixin = function() {
+    // Add $id before everyting
+    if (mix.inProduction()) {
+        return `
+            @mixin adapt-print {
+                #{'$id'} {
+                    @content;
+                }
+            }
+        `;
+    }
+
+    return `
+        @mixin adapt-print {
+            @content;
+        }
+    `;
+
+}
+
 /*
  * Build script and compile sass
  * Display banner and hot reload
  */
 mix.js('src/Scripts/main.js', 'dist/snippet.js')
-   .sass('src/Style/main.scss', 'dist/snippet.css')
+   .sass('src/Style/main.scss', 'dist/snippet.css', {
+       data: buildAdaptMixin(),
+   })
    .browserSync();
