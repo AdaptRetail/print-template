12169f9f08192a18e3a7d3353ce0484bd77a6ebf
Lasse <lasse@haslev.no>
2017-11-22 23:13:15 +0100

    Added adapt-production and adapt-development mixin helpers

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,8 +6,19 @@ let mix = require( 'laravel-mix' );
 require( '@adapt-retail/adapt-mix-extender' ).extend( mix );
 
 /**
+ * Helper for doing different things when
+ * running "npm run watch" and "npm run prod"
+ *
+ * adapt-print
  * Add $id before everything when running
  * npm run prod
+ *
+ * adapt-production
+ * Only show content in production
+ *
+ * adapt-development
+ * Only show content in development
+ *
  */
 var buildAdaptMixin = function() {
     if (mix.inProduction()) {
@@ -17,11 +28,27 @@ var buildAdaptMixin = function() {
                     @content;
                 }
             }
+            @mixin adapt-production {
+                @content;
+            }
+            @mixin adapt-development {
+                #not-used {
+                    @content;
+                }
+            }
         `;
     }
 
     return `
         @mixin adapt-print {
+            @content;
+        }
+        @mixin adapt-production {
+            #not-used {
+                @content;
+            }
+        }
+        @mixin adapt-development {
             @content;
         }
     `;
